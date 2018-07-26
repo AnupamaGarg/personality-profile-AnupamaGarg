dbbe3bf564f16e6c838bc61178efe305d96ec77f
anupamagarg <dolly.singla@gmail.com>
2018-07-26 08:59:46 -0500

    Meg

---
+++ data.js
@@ -0,0 +1,69 @@
+const personalityProfile = {
+    GeneralProfile: {
+        Name: "Anupama Garg",
+        Job: "NSS Student",
+        Agreewith: [
+            " I believe in realistic and practical approch ",
+            " I believe in respecting others ideas and beliefs",
+            " I believe in Spending time coming up with detail plan"
+        ],
+        NotAgreewith: [
+            "Todo list is not most important because it helps to organise your actions. ",
+            " ",
+            " "
+
+        ]
+
+
+
+    },
+    PeopleLikeMe: ["Seema Sharma(friend)",
+        "Sulbha Bansal(sister)",
+        "Kiran Saini (colleague)",
+        "A.L Garg (myFather)"],
+
+
+
+
+    Comunication: [
+        "1. I would like to communicate with my little sib the way he/she feels comfortable, like one to one, on slack, through email etc",
+        "2. I would like to have efficient , positive and professional communication with instructors whether it is one to one , through group discussion, or via email,slack. etc",
+        "3. I would like to communicate with my teammates by listening to them respecting their feelings and have efficient positive communication."
+
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+const saveDatabase = function (databaseObject, localStorageKey) {
+    /*
+        Convert the Object into a string.
+    */
+    const stringifiedDatabase = JSON.stringify(databaseObject)
+
+    /*
+        Create a key in local storage, and store the string
+        version of your inventory database as the value
+    */
+    localStorage.setItem(localStorageKey, stringifiedDatabase)
+}
+saveDatabase(personalityProfile, "personalInfo");
+
+const loadDatabase = function (localStorageKey) {
+    // Get the string version of the database
+    const databaseString = localStorage.getItem(localStorageKey)
+
+    // Use JSON.parse() to convert the string back into an object
+    return JSON.parse(databaseString)
+}
+console.log(loadDatabase("personalInfo"));
+
+
+
--- personal.js
+++ personal.js
@@ -2,17 +2,53 @@ const databaseString = localStorage.getItem("personalInfo")
 const data = JSON.parse(databaseString)
 
 var mainInformation = document.querySelector(".showProfile")
-let PersonalInfo = data.GeneralProfile
-  console.log(PersonalInfo);  
-  console.log(data);
+// let PersonalInfo = data.GeneralProfile
+// console.log(PersonalInfo);
+// console.log(data);
 
-  let addPersonalInfo = function(PersonalInfo){
+// let addPersonalInfo = function (PersonalInfo) {
 
-    let myInfo = document.querySelector(".personalInfo").innerHTML = `
-    
-    <h5 class = "name "><b>Name :    ${PersonalInfo.}</b></h5>
-    <h5 class = "job"><b>Job:    ${PersonalInfo.}</b></h5>
- 
- 
-  }
-  addPersonalInfo(PersonalInfo);
+//     let myInfo = document.querySelector(".personalInfo").innerHTML =
+
+//         `<h5 class = "name"><b>Name:    ${PersonalInfo.Name}</b></h5>`
+//             `<h5  class = "job" ><b>Job:    ${PersonalInfo.Job}</b></h5>`
+//             `<h4 ><b> Agree With</b> </h4> <ul class = "AgreeWithList"></ul>`
+//     for (let i = 0; i< PersonalInfo.Agreewith.length; i++) {
+//         const currentValue = PersonalInfo.Agreewith[i];
+//         document.querySelector(".AgreeWithList").innerHTML += `<li> <b>${currentValue}</b></li>`
+//     }
+//     `<h4 ><b> Not Agree With</b> </h4> <ul class = " NotAgreeWithList"></ul>`
+//     for (let y = 0; y < PersonalInfo.NotAgreewith.length; y++) {
+//         const current2Value = PersonalInfo.NotAgreewith[y];
+//         document.querySelector(".AgreeWithList").innerHTML += `<li> <b>${current2Value}</b></li>`
+
+//     }
+// }
+// addPersonalInfo(PersonalInfo);
+// -------------------------------------------------------------
+// let AlikePeople = data. PeopleLikeMe ;
+// let addAlikePeople = function (AlikePeople) {
+
+//     let myAlike = document.querySelector(".alikePeople").innerHTML =
+//         `<h4 ><b> People like Me</b> </h4> <ul class = "alikeList"></ul>`
+//     for (let i = 0; i < AlikePeople.PeopleLikeMe.length; i++) {
+//         const currentPerson = AlikePeople.PeopleLikeMe[i];
+//         document.querySelector(".alikePeople").innerHTML += `<li> <b>${currentPerson}</b></li>`
+
+//     }
+// }
+// addAlikePeople(AlikePeople);
+
+// --------------------------------------------------------------
+let ComunicatePreference = data.Comunication ;
+let addComunication = function () {
+
+    let comPreference = document.querySelector(".comunicateHTML").innerHTML =
+        `<h4 ><b> Comunication</b> </h4> <ul class = "comList"></ul>`
+    for (let i = 0; i < ComunicatePreference.Comunication.length; i++) {
+        const currentCom = ComunicatePreference.Comunication[i];
+        document.querySelector(".comList").innerHTML += `<li> <b>${currentCom}</b></li>`
+    }
+}
+addComunication(ComunicatePreference)
+
